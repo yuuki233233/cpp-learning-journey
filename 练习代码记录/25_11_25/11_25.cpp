1155309c3454238376e3eb92fa39c9b30a8ed067
Yuuki <yuuki233233@Outlook.com>
2025-11-25 14:58:37 +0800

    添加练习代码

---
+++ 练习代码记录/25_11_25/11_25.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+//C++升级struct升级成了类
+//1、类里面可以定义函数
+//2、struct名称就可以代表类型
+
+//C++兼容C中struct的用法
+typedef struct ListNodeC
+{
+	struct ListNOdeC* next;
+	int val;
+}LTNode;
+
+//C++中不再需要typedef，ListNodeCPP就可以代表类型
+struct ListNodeCPP
+{
+	//C++中，struct可以定义函数
+	void Init(int x)
+	{
+		next = nullptr;
+		val = x;
+	}
+
+	//不需要用struct ListNodeCPP
+	ListNodeCPP* next;
+	int val;
+};
+
+int main()
+{
+	return 0;
+}
