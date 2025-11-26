03127aa0f30d26e543cee0cb80a992925ab5f826
Yuuki <yuuki233233@Outlook.com>
2025-11-26 21:36:24 +0800

    添加练习代码

--- 练习代码记录/25_11_26/拷贝构造函数.cpp
+++ 练习代码记录/25_11_26/拷贝构造函数.cpp
@@ -122,53 +122,134 @@
 //	return 0;
 //}
 
+//#include<iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "/" << _month << "/" << _day << endl;
+//	}
+//
+//	//这里必须是引用，如果直接调用会导致无穷递归调用
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//void Func1(Date d)
+//{
+//	cout << &d << endl;
+//	d.Print();
+//}
+//
+//int main()
+//{
+//	Date d1(2025, 11, 16);
+//	d1.Print();
+//
+//	Func1(d1);
+//
+//	Date d2(d1);
+//	d2.Print();
+//
+//	return 0;
+//}
+
 #include<iostream>
 using namespace std;
 
-class Date
+typedef int STDataType;
+class Stack
 {
 public:
-	Date(int year = 1, int month = 1, int day = 1)
+	Stack(int n = 4)
 	{
-		_year = year;
-		_month = month;
-		_day = day;
+		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+		if (nullptr == _a)
+		{
+			perror("malloc申请空间失败");
+			return;
+		}
+		_capacity = n;
+		_top = 0;
 	}
 
-	void Print()
+	// st2(st1)
+	Stack(const Stack& st)
 	{
-		cout << _year << "/" << _month << "/" << _day << endl;
+		cout << "Stack(const Stack& st)" << endl;
+
+		// 需要对_a指向资源创建同样大的资源再拷贝值
+		_a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+		if (nullptr == _a)
+		{
+			perror("malloc申请空间失败!!!");
+			return;
+		}
+		memcpy(_a, st._a, sizeof(STDataType) * st._top);
+		_top = st._top;
+		_capacity = st._capacity;
 	}
 
-	//这里必须是引用，如果直接调用会导致无穷递归调用
-	Date(const Date& d)
+	void Push(STDataType x)
 	{
-		_year = d._year;
-		_month = d._month;
-		_day = d._day;
+		if (_top == _capacity)
+		{
+			int newcapacity = _capacity * 2;
+			STDataType* tmp = (STDataType*)realloc(_a, newcapacity *
+				sizeof(STDataType));
+			if (tmp == NULL)
+			{
+				perror("realloc fail");
+				return;
+			}
+			_a = tmp;
+			_capacity = newcapacity;
+		}
+		_a[_top++] = x;
+
 	}
 
+	~Stack()
+	{
+		cout << "~Stack()" << endl;
+		free(_a);
+		_a = nullptr;
+		_top = _capacity = 0;
+	}
 private:
-	int _year;
-	int _month;
-	int _day;
+	STDataType* _a;
+	size_t _capacity;
+	size_t _top;
 };
-
-void Func1(Date d)
-{
-	cout << &d << endl;
-	d.Print();
-}
 
 int main()
 {
-	Date d1(2025, 11, 16);
-	d1.Print();
+	Stack st1;
+	st1.Push(1);
+	st1.Push(2);
 
-	Func1(d1);
-
-	Date d2(d1);
-	d2.Print();
+	// Stack不显示实现拷贝构造，用自动生成的拷贝构造完成浅拷贝
+	// 会导致st1和st2里面的_a指针指向同一块资源，析构时会析构两次，程序崩溃
+	Stack st2(st1);
 
 	return 0;
 }
