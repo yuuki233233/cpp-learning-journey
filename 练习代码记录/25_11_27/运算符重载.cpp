55f7332bdcf165966c629a4b4675b33eddd65078
Yuuki <yuuki233233@Outlook.com>
2025-11-27 22:46:24 +0800

    add

--- 练习代码记录/25_11_27/运算符重载.cpp
+++ 练习代码记录/25_11_27/运算符重载.cpp
@@ -46,6 +46,64 @@
 //	return 0;
 //}
 
+//#include<iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "/" << _month << "/" << _day << endl;
+//	}
+//
+//	//重载未全局的面临对象访问私有成员变量的问题
+//	bool operator==(const Date& d)
+//	{
+//		return _year == d._year
+//			&& _month == d._month
+//			&& _day == d._day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1(2025, 11, 26);
+//	Date d2(2025, 11, 27);
+//
+//	//运算符重载函数可以显示调用
+//	//operator==(d1, d2);
+//
+//
+//	//编译器会转变为operator==(d1, d2);
+//	//d1 == d2;
+//
+//
+//	//运算符重载函数可以显示调用
+//	d1.operator==(d2);
+//
+//	//编译器会转变为d1.operator==(d2);
+//	d1 = d2;
+//
+//	Date d3(d2);
+//	Date d4 = d3;
+//
+//	return 0;
+//}
+
+
 #include<iostream>
 using namespace std;
 
@@ -83,19 +141,12 @@ int main()
 	Date d1(2025, 11, 26);
 	Date d2(2025, 11, 27);
 
-	//运算符重载函数可以显示调用
-	//operator==(d1, d2);
+	//赋值重载
+	d2 = d1;
 
-
-	//编译器会转变为operator==(d1, d2);
-	//d1 == d2;
-
-
-	//运算符重载函数可以显示调用
-	d1.operator==(d2);
-
-	//编译器会转变为d1.operator==(d2);
-	d1 == d2;
+	//拷贝构造
+	Date d3(d2);
+	Date d4 = d3;
 
 	return 0;
 }
