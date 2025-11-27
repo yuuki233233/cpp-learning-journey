39025c8e307a110417d283b3067e9fe7b48ec283
Yuuki <yuuki233233@Outlook.com>
2025-11-28 00:29:17 +0800

    推断日期

---
+++ 练习代码记录/日期/Date.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Date.h"
+
+Date& Date::operator+=(int day)
+{
+	_day += day;
+	while (_day > GetMonthDay(_year, _month))
+	{
+		_day -= GetMonthDay(_year, _month);
+		_month++;
+
+		if (_month == 13)
+		{
+			_year++;
+			_month = 1;
+		}
+	}
+
+	return *this;
+}
+
+Date Date::operator+(int day)
+{
+	Date tmp = *this;
+	tmp += day;
+	
+	return tmp;
+}
