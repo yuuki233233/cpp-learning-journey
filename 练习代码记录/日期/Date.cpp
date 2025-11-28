52918365a1ae8f59697bc20d2bd3a8c70d8d79b9
Yuuki <yuuki233233@Outlook.com>
2025-11-28 10:22:18 +0800

    实现相减得日期

--- 练习代码记录/日期/Date.cpp
+++ 练习代码记录/日期/Date.cpp
@@ -26,3 +26,40 @@ Date Date::operator+(int day)
 	
 	return tmp;
 }
+
+Date& Date::operator-=(int day)
+{
+	_day -= day;
+	while(_day < GetMonthDay(_year, _month))
+	{
+		_day += GetMonthDay(_year, _month);
+		_month--;
+		if (_month == 0)
+		{
+			_year--;
+			_month = 12;
+		}
+	}
+	
+	while (_day > GetMonthDay(_year, _month))
+	{
+		_day -= GetMonthDay(_year, _month);
+		_month++;
+
+		if (_month == 13)
+		{
+			_year++;
+			_month = 1;
+		}
+	}
+
+	return *this;
+}
+
+Date Date::operator-(int day)
+{
+	Date tmp = *this;
+	tmp -= day;
+
+	return tmp;
+}
