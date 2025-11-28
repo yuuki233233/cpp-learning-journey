42bacfce37765c5f586231f88694057446f174fd
Yuuki <yuuki233233@Outlook.com>
2025-11-28 17:59:28 +0800

    添加了流成员，可多个输入输出

--- 练习代码记录/日期/test.cpp
+++ 练习代码记录/日期/test.cpp
@@ -41,9 +41,32 @@ void Test04()
 	cout << d1 - d2 << endl;
 }
 
+void Test05()
+{
+	Date d1(2025, 11, 27);
+	Date d2(2025, 11, 28);
+	//倒反天罡(在成员函数中，this指针是d1，而out却在第二个中)
+	//d1 << cout;
+	//d1.operator<<(cout);
+
+	//返回值接收流，可以输出多个
+	cout << d1 << d2;
+}
+
+void Test06()
+{
+	Date d1;
+	Date d2;
+
+	cin >> d1 >> d2;
+	cout << d1 << d2;
+
+	cout << d1 - d2 << endl;
+}
+
 int main()
 {
-	Test04();
+	Test06();
 
 	return 0;
 }
