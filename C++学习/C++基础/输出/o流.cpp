ba5ba4e86f89c3e3077b7135e674895a65126e42
Yuuki <yuuki233233@Outlook.com>
2025-11-30 10:01:29 +0800

    输出流

---
+++ C++学习/C++基础/输出/o流.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	double b = 0.1;
+	char c = 'x';
+
+	cout << a << " " << b << " " << c << endl;
+	std::cout << a << " " << b << " " << c << std::endl;
+	return 0;
+}
