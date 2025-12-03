2f25b572bb21589857a496155e6298e53344c2f3
Yuuki <yuuki233233@Outlook.com>
2025-12-03 18:39:02 +0800

    add

--- 练习代码记录/25_12_2/模板.cpp
+++ 练习代码记录/25_12_2/模板.cpp
@@ -76,6 +76,40 @@ using namespace std;
 //	return 0;
 //}
 
+//// 专门处理int的加法函数
+//int Add(int left, int right)
+//{
+//	return left + right;
+//}
+//// 通用加法函数
+//template<class T>
+//T Add(T left, T right)
+//{
+//	return left + right;
+//}
+//void Test()
+//{
+//	Add(1, 2); // 与非模板函数匹配，编译器不需要特化
+//	Add<int>(1, 2); // 调用编译器特化的Add版本
+//}
+
+
+//// 专门处理int的加法函数
+//int Add(int left, int right)
+//{
+//	return left + right;
+//}
+//// 通用加法函数
+//template<class T1, class T2>
+//T1 Add(T1 left, T2 right)
+//{
+//	return left + right;
+//}
+//void Test()
+//{
+//	Add(1, 2); // 与非函数模板类型完全匹配，不需要函数模板实例化
+//	Add(1, 2.0); // 模板函数可以生成更加匹配的版本，编译器根据实参生成更加匹配的Add函数
+//}
 
 //-----------------------------------------------------
 //template<class T>
@@ -108,7 +142,7 @@ using namespace std;
 //	int _capacity;
 //};
 //
-////需要单独声明，不然编译器不认识上面模板的类型
+//// 模版不建议声明和定义分离到两个文件.h 和.cpp会出现链接错误
 //template<class T>
 //void Stack<T>::Push(const T& x)
 //{
