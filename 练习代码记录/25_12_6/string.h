1b3c93c7ee01cd43e1cb7de3d11ed179d8a4afff
Yuuki <yuuki233233@Outlook.com>
2025-12-06 22:48:46 +0800

    添加string模拟实现

---
+++ 练习代码记录/25_12_6/string.h
@@ -0,0 +1,101 @@
+#pragma once
+#include<iostream>
+#include<assert.h>
+using namespace std;
+ 
+namespace yuuki
+{
+	class string
+	{
+	public:
+		//用迭代器时
+		typedef char* iterator;
+		typedef const char* const_iterator;
+
+		iterator begin()
+		{
+			return _str;
+		}
+
+		iterator end()
+		{
+			return _str + _size;
+		}
+
+		// 方法1：
+		//string()
+		//	:_str(new char[1]{'\0'})
+		//	,_size(0)
+		//	,_capacity(0)
+		//{ }
+
+		//string(const char* str)
+		//{
+		//	_size = strlen(str);
+		//	_capacity = _size;
+		//	// _capacity不包含\0
+		//	_str = new char[_capacity + 1];
+		//	strcpy(_str, str);
+		//}
+
+		// 方法2：合并成一个（缺省参数）
+		string(const char* str = " ")
+		{
+			_size = strlen(str);
+			_capacity = _size;
+			_str = new char[_capacity + 1];
+			strcpy(_str, str);
+		}
+
+		~string()
+		{
+			delete[] _str;
+			_str == nullptr;
+			_size = _capacity = 0;
+		}
+
+		const char* c_str() const
+		{
+			return _str;
+		}
+
+		size_t size() const
+		{
+			return _size;
+		}
+
+		size_t capacity() const
+		{
+			return _capacity;
+		}
+
+		char& operator[](size_t pos)
+		{
+			assert(pos < _size);
+
+			return _str[pos];
+		}
+
+		const char& operator[](size_t pos) const
+		{
+			assert(pos < _size);
+
+			return _str[pos];
+		}
+
+		void reserve(size_t n);
+		void push_back(char ch);
+		void append(const char* str);
+		string& operator+=(char ch);
+		string& operator+=(const char* str);
+
+
+	private:
+		char* _str;
+		int _size;
+		int _capacity;
+	};
+
+	void test_string1();
+	void test_string2();
+}
