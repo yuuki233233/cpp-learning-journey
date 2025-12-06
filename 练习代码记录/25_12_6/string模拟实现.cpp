1d14db023fee5a3fd93b88b2790563531757b005
Yuuki <yuuki233233@Outlook.com>
2025-12-06 23:30:42 +0800

    添加string模拟实现

--- 练习代码记录/25_12_6/string模拟实现.cpp
+++ 练习代码记录/25_12_6/string模拟实现.cpp
@@ -3,6 +3,8 @@
 
 namespace yuuki
 {
+	const size_t string::npos = -1;
+
 	void string::reserve(size_t n)
 	{
 		if (n > _capacity)
@@ -23,24 +25,63 @@ namespace yuuki
 		}
 		_str[_size] = ch;
 		++_size;
+		//别忘了加'\0'
 		_str[_size] = '\0';
 	}
 
 	void string::append(const char* str)
 	{
-		
+		size_t len = strlen(str);
+		if (_size + len > _capacity)
+		{
+			//需要多少扩多少
+			//reserve(_size + len);
+
+			//二倍扩容（避免频繁扩容）
+			reserve(_size + len > 2 * _capacity ? _size + len : 2 * _capacity);
+		}
+
+		strcpy(_str + _size, str);
+		_size += len;
 	}
 
+	//单字符
 	string& string::operator+=(char ch)
 	{
 		push_back(ch);
 		return *this;
 	}
 
-	/*string& string::operator+=(const char* str)
+	//字符串（不能按二倍扩容）
+	string& string::operator+=(const char* str)
 	{
-		
-	}*/
+		append(str);
+		return *this;
+	}
+
+	void string::insert(size_t pos, char ch)
+	{
+		assert(pos <= _size);
+
+		if (_size == _capacity)
+		{
+			reserve(_capacity == 0 ? 4 : _capacity * 2);
+		}
+
+		//挪动数据
+		size_t end = _size;
+		while (end >= pos)
+		{
+			_str[end + 1] = _str[end];
+			--end;
+		}
+
+		_str[pos] = ch;
+		++_size;
+	}
+
+	//void string::insert(size_t pos, const char* str);
+	//void string::erase(size_t pos, size_t);
 
 	void test_string1()
 	{
@@ -76,6 +117,12 @@ namespace yuuki
 		string s1("hello world");
 		s1 += 'x';
 		s1 += '#';
+		cout << s1.c_str() << endl;
+
+		s1 += "hello yuuki";
+		cout << s1.c_str() << endl;
+
+		s1.insert(5, '$');
 		cout << s1.c_str() << endl;
 	}
 }
