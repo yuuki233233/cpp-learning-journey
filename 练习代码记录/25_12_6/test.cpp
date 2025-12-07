94332622ada6ac9eb138ae89b24389c0b51af7da
Yuuki <yuuki233233@Outlook.com>
2025-12-07 23:00:47 +0800

    添加练习代码

--- 练习代码记录/25_12_6/test.cpp
+++ 练习代码记录/25_12_6/test.cpp
@@ -1,10 +1,99 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"string.h"
 
+namespace yuuki
+{
+	void test_string1()
+	{
+		string s1;
+		string s2("hello world");
+		cout << s1.c_str() << endl;
+		cout << s2.c_str() << endl;
+
+		for (size_t i = 0; i < s2.size(); ++i)
+		{
+			s2[i] += 2;
+		}
+		cout << s2.c_str() << endl;
+
+		for (auto e : s2)
+		{
+			cout << e << " ";
+		}
+		cout << endl;
+
+		//char* it = s2.begin();
+		string::iterator it = s2.begin();
+		while (it != s2.end())
+		{
+			cout << *it << " ";
+			++it;
+		}
+		cout << endl;
+	}
+
+	void test_string2()
+	{
+		string s1("hello world");
+		//运算符重载单字符
+		s1 += 'x';
+		s1 += '#';
+		cout << s1.c_str() << endl;
+
+		//运算符重载字符串
+		s1 += "hello yuuki";
+		cout << s1.c_str() << endl;
+
+		//插入单字符
+		s1.insert(0, '$');
+		cout << s1.c_str() << endl;
+
+		//插入字符串
+		s1.insert(6, "######");
+		cout << s1.c_str() << endl;
+	}
+
+	void test_string3()
+	{
+		string s1("hello world");
+		s1.erase(6, 100);
+		cout << s1.c_str() << endl;
+
+		string s2("hello world");
+		s2.erase(6);
+		cout << s2.c_str() << endl;
+
+		string s3("hello world");
+		s3.erase(6, 3);
+		cout << s3.c_str() << endl;
+
+		string s4("hello world");
+		s4.erase(2, 1);
+		cout << s4.c_str() << endl;
+	}
+
+	void test_string4()
+	{
+		string s1("test.cpp.zip");
+		size_t pos = s1.find('.');
+		string suffix = s1.substr(pos);
+		cout << suffix.c_str() << endl;
+
+		//检测编译器优化，如果编译器不优化会释放同一个空间
+		string s2(s1);
+		cout << suffix.c_str() << endl;
+
+		//默认调用浅拷贝，会导致内存泄漏
+		s1 = s2;
+
+	}
+}
+
 int main()
 {
 	// yuuki::test_string1(); 
-	yuuki::test_string2();
-
+	//yuuki::test_string2();
+	// yuuki::test_string3();
+	yuuki::test_string4();
 	return 0;
 }
