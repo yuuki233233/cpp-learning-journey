da5d3584457f206e13a6fa894388e68e350ce76a
Yuuki <yuuki233233@Outlook.com>
2025-12-11 23:37:28 +0800

    添加练习代码

--- 练习代码记录/25_12_10/test.cpp
+++ 练习代码记录/25_12_10/test.cpp
@@ -10,4 +10,51 @@ Date& Date::operator=(const Date& d)
 	return *this;
 }
 
+bool Date::operator>(const Date& d)
+{
+	if (_year > d._year)
+	{
+		return true;
+	}
+	else if (_year == d._year)
+	{
+		if (_month > d._month)
+		{
+			return true;
+		}
+		else if (_month == d._month)
+		{
+			return _day > d._day;
+		}
+	}
+	return false;
+}
 
+bool Date::operator==(const Date& d)
+{
+	return _year == d._year
+		&& _month == d._month
+		&& _day == d._day;
+}
+
+bool Date::operator>=(const Date& d)
+{
+	return (*this > d || *this == d);
+}
+
+bool Date::operator<=(const Date& d)
+{
+	return !(*this > d);
+}
+
+bool Date::operator<(const Date& d)
+{
+	return !(*this >= d);
+}
+
+
+
+bool Date::operator!=(const Date& d)
+{
+	return !(*this == d);
+}
