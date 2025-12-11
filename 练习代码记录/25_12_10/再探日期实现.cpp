601dfd51b4467933e742aca8dba8099623aa1e53
Yuuki <yuuki233233@Outlook.com>
2025-12-11 22:24:34 +0800

    添加练习代码

--- 练习代码记录/25_12_10/再探日期实现.cpp
+++ 练习代码记录/25_12_10/再探日期实现.cpp
@@ -1,9 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"date.h"
 
+void test_date01()
+{
+	Date d1(2025, 12, 10);
+	d1.Print();
+
+	Date d2(2007, 6, 18);
+	d2.Print();
+
+	//运算符重载
+	d1 = d2;
+	d1.Print();
+
+	//拷贝构造
+	Date d3 = d1;
+	d3.Print();
+}
+
 int main()
 {
-	yuuki::test_date01();
+	test_date01();
 
 	return 0;
 }
