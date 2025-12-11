6480fa48817e627799b5071c9052e3a6438e3aea
Yuuki <yuuki233233@Outlook.com>
2025-12-12 00:18:32 +0800

    再次完成日期代码实现

--- 练习代码记录/25_12_10/再探日期实现.cpp
+++ 练习代码记录/25_12_10/再探日期实现.cpp
@@ -17,19 +17,35 @@ void test_date01()
 	Date d3 = d1;
 	d3.Print();
 }
-
+	
 void test_date02()
 {
 	Date d1(2025, 12, 10);
 	Date d2(2025, 12, 10);
 
-	
+	d1 += 100;
+	d1.Print();
+
+	d2 -= 100;
+	d2.Print();
+}
+
+void test_date03()
+{
+	Date d1(2025, 12, 10);
+	Date d2(2007, 6, 18);
+
+	cout << d1 << d2;
+
+	cin >> d1 >> d2;
+	cout << d1 << d2;
 }
 
 int main()
 {
 	//test_date01();
-	test_date02();
+	//test_date02();
+	test_date03();
 
 	return 0;
 }
