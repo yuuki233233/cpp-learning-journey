3329674c418d4e51b8664e3f1ea274c79368780d
Yuuki <yuuki233233@Outlook.com>
2025-12-20 12:45:30 +0800

    添加模拟实现list代码

--- 练习代码记录/25_12_17/模拟实现list.cpp
+++ 练习代码记录/25_12_17/模拟实现list.cpp
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include"list.h"
+//#include"list.h"
+#include"second_list.h"
 
 int main()
 {
-	yuuki::test_list01();
+	//yuuki::test_list01();
+	yuuki::test_list02();
 
 	return 0;
 }
