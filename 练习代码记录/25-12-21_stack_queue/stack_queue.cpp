1666aad5f7089cbe84bdf734fcbb8a2e7b667e6b
Yuuki <yuuki233233@Outlook.com>
2025-12-21 23:30:27 +0800

    完成string模拟实现

---
+++ 练习代码记录/25-12-21_stack_queue/stack_queue.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+namespace yuuki
+{
+	template<class T>
+	class stack
+	{
+
+	};
+
+	template<class T>
+	class queue
+	{
+
+	};
+}
---
+++ 练习代码记录/25-12-21_stack_queue/stack_queue.h
@@ -0,0 +1,9 @@
+#pragma once
+#include"stack_queue.h"
+
+int main()
+{
+
+
+	return 0;
+}
