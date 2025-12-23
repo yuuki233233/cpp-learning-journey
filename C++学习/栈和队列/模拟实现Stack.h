08b0cf1db47ffaa19d08462057fd2f807a9ddbd1
Yuuki <yuuki233233@Outlook.com>
2025-12-23 13:19:45 +0800

    模拟实现栈和队列

---
+++ C++学习/栈和队列/模拟实现Stack.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<deque>
+
+namespace yuuki
+{
+	template<class T, class Container = deque<T>>
+	class stack
+	{
+	public:
+		void push(const T& x)
+		{
+			_con.push_back(x);
+		}
+
+		void pop()
+		{
+			_con.pop_back();
+		}
+
+		const T& top() const
+		{
+			return _con.back();
+		}
+
+		size_t size() const
+		{
+			return _con.size();
+		}
+
+		bool empty() const
+		{
+			return _con.empty();
+		}
+
+	private:
+		Container _con;
+	};
+}
