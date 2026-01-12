a21186ca0369753fa2de59be1d836397ef615344
Yuuki <yuuki233233@Outlook.com>
2026-01-12 22:40:40 +0800

    多态

---
+++ 练习代码记录/26_01_12/多态.cpp
@@ -0,0 +1,144 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+//class A
+//{
+//public:
+//	virtual void func() { cout << "A" << endl; }
+//};
+//
+//class B : public A // 派生类B继承基类A
+//{
+//public:
+//	virtual void func() { cout << "B" << endl; } // 带上虚函数，返回类型、函数名相同
+//};
+//
+//void test(A* a) // 基类的指针或引用
+//{
+//	a->func();
+//}
+//
+//int main()
+//{
+//	A a;
+//	B b;
+//	test(&a); // 函数是基类的指针传引用 - 函数是基类的引用则传变量
+//	test(&b); // 函数是基类的指针传引用 - 函数是基类的引用则传变量
+//
+//	return 0;
+//}
+//
+//
+//class Animal
+//{
+//public:
+//	virtual void talk() const
+//	{ }
+//};
+//
+//class Cat : public Animal
+//{
+//public:
+//	virtual void talk() const
+//	{
+//		cout << "miao~" << endl; // 带上虚函数，返回类型、函数名相同
+//	}
+//};
+//
+//class Dog : public Animal
+//{
+//public:
+//	virtual void talk() const
+//	{
+//		cout << "wang~" << endl; // 带上虚函数，返回类型、函数名相同
+//	}
+//};
+//
+//void func(Animal* a) // 基类的指针或引用
+//{
+//	a->talk();
+//}
+//
+//int main()
+//{
+//	Cat cat;
+//	Dog dog;
+//	func(&cat); // 函数是基类的指针传引用 - 函数是基类的引用则传变量
+//	func(&dog); // 函数是基类的指针传引用 - 函数是基类的引用则传变量
+//
+//	return 0;
+//}
+
+
+//class A
+//{
+//public:
+//	virtual void func(int val = 1) { cout << "A->" << val << endl; }
+//	virtual void test() { func(); }
+//};
+//
+//class B : public A
+//{
+//public:
+//	void func(int val = 0) { cout << "B->" << val << endl; }
+//};
+//
+//int main(int argc, char* argv[])
+//{
+//	B* p = new B;
+//	p->test();
+//
+//	return 0;
+//}
+
+
+//————————————————————————————————————————协变
+
+class A {}; // 基类
+class B : public A {}; // 派生类继承基类
+
+class Animal
+{
+public:
+	virtual A* func()
+	{
+		return nullptr; // 虚函数必须返回一个值
+	}
+};
+
+class Cat : public Animal
+{
+public:
+	virtual B* func()
+	{
+		cout << "miao~" << endl;
+		return nullptr; // 虚函数必须返回一个值
+	}
+};
+
+class Dog : public Animal
+{
+public:
+	virtual B* func()
+	{
+		cout << "wan~" << endl;
+		return nullptr; // 虚函数必须返回一个值
+	}
+};
+
+void test(Animal* a) // 基类的指针或引用
+{
+	a->func();
+}
+
+int main()
+{
+	Cat cat;
+	Dog dog;
+
+	test(&cat); // 引用
+	test(&dog); // 引用
+
+	return 0;
+}
