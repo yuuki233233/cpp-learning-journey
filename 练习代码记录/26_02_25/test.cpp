e1e2f46dcec707fdac6894fa9ba3196d1aacf163
Yuuki <yuuki233233@Outlook.com>
2026-02-25 22:11:51 +0800

    Lambda

---
+++ 练习代码记录/26_02_25/test.cpp
@@ -0,0 +1,65 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+struct Goods
+{
+	string _name; // 名字
+	double _price; // 价格
+	int _evaluate; // 评价
+	// ...
+
+	Goods(const char* str, double price, int evaluate)
+		:_name(str)
+		, _price(price)
+		, _evaluate(evaluate)
+	{
+	}
+};
+
+struct ComparePriceLess
+{
+	bool operator()(const Goods& gl, const Goods& gr)
+	{
+		return gl._price < gr._price;
+	}
+};
+
+struct ComparePriceGreater
+{
+	bool operator()(const Goods& gl, const Goods& gr)
+	{
+		return gl._price > gr._price;
+	}
+};
+
+int main()
+{
+	vector<Goods> v = { { "苹果", 2.1, 5 }, { "⾹蕉", 3, 4 }, { "橙⼦", 2.2, 3
+	}, { "菠萝", 1.5, 4 } };
+
+	// 类似这样的场景，我们实现仿函数对象或者函数指针⽀持商品中
+	// 不同项的⽐较，相对还是⽐较⿇烦的，那么这⾥lambda就很好⽤了
+	sort(v.begin(), v.end(), ComparePriceLess());
+	sort(v.begin(), v.end(), ComparePriceGreater());
+
+	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {
+		return g1._price < g2._price;
+		});
+
+	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {
+		return g1._price > g2._price;
+		});
+
+	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {
+		return g1._evaluate < g2._evaluate;
+		});
+
+	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2) {
+		return g1._evaluate > g2._evaluate;
+		});
+
+	return 0;
+}
